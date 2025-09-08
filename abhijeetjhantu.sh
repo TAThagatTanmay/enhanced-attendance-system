eb793e1bce52612e342dd990f0bfa01844550d60
TAThagatTanmay <tathagattanmayall@gmail.com>
2025-09-08 18:30:54 +0530

    Create abhijeetjhantu.sh

---
+++ abhijeetjhantu.sh
@@ -0,0 +1,13 @@
+pip install numpy
+pip install python-dotenv
+pip install setuptools
+pip install wheel
+pip install PyJWT
+pip install opencv-python-headless
+pip install face-recognition
+pip install dlib
+pip install python-multipart
+pip install psutil
+pip install scikit-learn
+pip install matplotlib
+pip install pandas
